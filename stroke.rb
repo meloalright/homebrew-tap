5c547a0d1a945a405b4b19bd17d1b97bbd2df724
MeloAlright <meloalright@gmail.com>
2022-04-28 23:58:12 +0800

    Update stroke.rb

--- stroke.rb
+++ stroke.rb
@@ -1,8 +1,8 @@
 class Stroke < Formula
   desc "A MacOS command stroke tool written in Rust"
   homepage "https://github.com/meloalright/stroke"
-  url "https://github.com/meloalright/stroke/releases/download/0.1.3-alpha/stroke.zip"
-  sha256 "bed44f739420ff161717dae6898044c74ec21ed4f6d65f8f9e054cc5c2cdf842"
+  url "https://github.com/meloalright/stroke/releases/download/0.1.3/stroke.zip"
+  sha256 "8aa11c7751954b25719d17ca5c7ed05c78a5bad89aefe6c6c501dcf578e98701"
   license "MIT"
 
   def install
